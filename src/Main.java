40ec88b62b16c2875b904fd0055480ff0722a346
Marco Pontes da Paz <marcopontes@uol.com.br>
2024-08-05 23:34:42 -0300

    primeira versão

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import br.marco.chamadas.ConsultarCep;
+import br.marco.modelos.CEP;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String cepConsulta = new String ("");
+
+        while (true) {
+
+            cepConsulta = lerCEP();
+
+            if (cepConsulta.compareTo("fim") == 0) {
+                break;
+            } else {
+
+                ConsultarCep consultarCep = new ConsultarCep();
+                CEP cep = consultarCep.consultar(cepConsulta);
+
+                System.out.println(cep);
+            }
+        }
+    }
+
+    public static String lerCEP () {
+
+        Scanner leitor = new Scanner (System.in);
+        String cep = new String ();
+
+        System.out.printf("Digite um CEP :");
+        cep =  leitor.nextLine();
+
+        return cep;
+    }
+}
---
+++ src/br/marco/chamadas/ConsultarCep.java
@@ -0,0 +1,55 @@
+package br.marco.chamadas;
+
+import br.marco.modelos.CEP;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultarCep {
+
+    public CEP consultar (String cepConsulta) {
+
+        CEP cep = new CEP();
+
+        if (cepConsulta.length() != 8) {
+            System.out.println("Formato de CEP inválido");
+        } else {
+
+            String url = "http://viacep.com.br/ws/" +
+                    cepConsulta +
+                    "/json";
+            try {
+
+                // constrói a requisição
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+                if (response.statusCode() == 400) {
+                    System.out.println("CEP inexistente !");
+                } else {
+
+                    String jsonBody = response.body();
+
+                    Gson gson = new Gson();
+                    cep = gson.fromJson(jsonBody, CEP.class);
+
+                    System.out.printf ("CEP == %s", cep);
+                    //System.out.println(jsonBody);
+
+                }
+
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return cep;
+    }
+}

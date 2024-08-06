43982ca2f9f97d10aea696714e5de793c2435adb
Marco Pontes da Paz <marcopontes@uol.com.br>
2024-08-06 00:10:44 -0300

    criando classe consultarCEP.

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ import br.marco.modelos.CEP;
 //import com.google.gson.Gson;
 //import com.google.gson.GsonBuilder;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+//import java.io.IOException;
+//import java.net.URI;
+//import java.net.http.HttpClient;
+//import java.net.http.HttpRequest;
+//import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Main {
